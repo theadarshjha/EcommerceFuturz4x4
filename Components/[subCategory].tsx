b24c5c813f68d43a075e860e0cc4d58ba8724a95
thejhaadarsh <thejhaadarsh@gmail.com>
2024-08-22 12:33:28 +0530

    error fixed

--- Components/[subCategory].tsx
+++ Components/[subCategory].tsx
@@ -1,11 +1,9 @@
-// pages/components/[subCategory].tsx
-import { useRouter } from 'next/navigation';
+import { useParams } from 'next/navigation';
 import FilterItems from '@/Components/FilterItems';
 import { SubCategoryType } from '@/Components/Types/Filter';
 
 const SubCategoryPage = () => {
-  const router = useRouter();
-  const { subCategory } = router.query;
+  const { subCategory } = useParams();
 
   if (!subCategory || typeof subCategory !== 'string') {
     return <div>Loading...</div>;
