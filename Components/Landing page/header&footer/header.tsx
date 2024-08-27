42efe64990687151334a17cba29a6de35a4cfca9
thejhaadarsh <thejhaadarsh@gmail.com>
2024-08-27 11:53:15 +0530

    new

--- Components/Landing page/header&footer/header.tsx
+++ Components/Landing page/header&footer/header.tsx
@@ -198,7 +198,7 @@ export default function Example() {
                       {category.featured.map((item) => (
                         <div key={item.name} className="group relative text-sm">
                           <div className="aspect-h-1 aspect-w-1 overflow-hidden rounded-lg bg-gray-100 group-hover:opacity-75">
-                           
+                            <Image alt={item.imageAlt} src={item.imageSrc} className="object-cover object-center" />
                           </div>
                           <a href={item.href} className="mt-6 block font-medium text-gray-900">
                             <span aria-hidden="true" className="absolute inset-0 z-10" />
@@ -260,7 +260,11 @@ export default function Example() {
 
             <div className="border-t border-gray-200 px-4 py-6">
               <a href="#" className="-m-2 flex items-center p-2">
-              
+                <Image
+                  alt=""
+                  src="https://tailwindui.com/img/flags/flag-canada.svg"
+                  className="block h-auto w-5 flex-shrink-0"
+                />
                 <span className="ml-3 block text-base font-medium text-gray-900">CAD</span>
                 <span className="sr-only">, change currency</span>
               </a>
